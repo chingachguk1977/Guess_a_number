d8e7e87ceba8b6e953af8ef0d286a3d9ed429f1a
Roman Bykov <97618922+chingachguk1977@users.noreply.github.com>
2022-01-30 21:29:06 +0300

    it works

--- game.py
+++ game.py
@@ -1,4 +1,6 @@
 from numpy import random
+from numpy import mean
+
 
 def bin_search(range_: list, num2guess: int) -> int:
     """Implements the binary search algorithm on the given sequence. This algorithm
@@ -29,29 +31,30 @@ def bin_search(range_: list, num2guess: int) -> int:
             lower_bound = mid + 1
     return None
 
-
-def score_game(random_predict) -> int:
-    """За какое количество попыток в среднем за 1000 подходов угадывает наш алгоритм
-
-    Args:
-        random_predict ([type]): функция угадывания
-
-    Returns:
-        int: среднее количество попыток
+def run_game():
     """
+    The main() function to run the game
+    """
+    
+    # the list to store the number of tries for each number
+    counters = []
+    
+    # random-generate the list of numbers
+    random_nums = [i for i in range(random.randint(1, 1000))]
+    
+    # the list must be sorted for bin search to work
+    random_nums.sort()
 
-    count_ls = [] # список для сохранения количества попыток
-    random.seed(1) # фиксируем сид для вопроизводимости
-    random_array = random.randint(1, 101, size=(1000)) # загадали список чисел
+    # iterating thro the random numbers
+    for number in random_nums:
+        counters.append(bin_search(random_nums, number))
 
-    for number in random_array:
-        count_ls.append(random_predict(number))
+    # calculate the mean for all numbers of tries
+    score = int(mean(counters))
 
-    score = int(np.mean(count_ls)) # находим среднее количество попыток
+    print(f'The average number of tries your algorithm guesses a number is: {score}')
 
-    print(f'Ваш алгоритм угадывает число в среднем за: {score} попыток')
-    return(score)
 
 if __name__ == '__main__':
-    # RUN
-    score_game(random_predict)
+    # run
+    run_game()
