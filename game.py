82b9d1eeff7ab6763c3f77b1295f2548e39ddf0e
Roman Bykov <97618922+chingachguk1977@users.noreply.github.com>
2022-01-30 21:07:44 +0300

    added bin search function

--- game.py
+++ game.py
@@ -1,23 +1,33 @@
-import numpy as np
+from numpy import random
 
-def random_predict(number: int = 1) -> int:
-    """Сам загадывает число  и сам угадывает
+def bin_search(range_: list, num2guess: int) -> int:
+    """Implements the binary search algorithm on the given sequence. This algorithm
+    guarantees the number being guessed will be found in no more than log2 of the 
+    number of elements in the given sequence. Make sure the sequence is sorted.
 
     Args:
-        number (int, optional): загаданное число. Defaults to 1.
+        range_ (list): A SORTED list sequence to search the number being guessed.
+        num2guess (int): The number to be guessed.
 
     Returns:
-        int: кол-во попыток
+        [int]: How many tries it took to guess the number correctly.
     """
-
+    
+    lower_bound = 0
+    upper_bound = len(range_) - 1
     count = 0
 
-    while True:
+    while lower_bound <= upper_bound:
         count += 1
-        predict_number = np.random.randint(1, 101)  # предполагаемое число
-        if number == predict_number:
-            break  # выход из цикла, если угадали
-    return count
+        mid = (lower_bound + upper_bound) // 2
+        guess = range_[mid]
+        if guess == num2guess:
+            return count
+        elif guess > num2guess:
+            upper_bound = mid - 1
+        else:
+            lower_bound = mid + 1
+    return None
 
 
 def score_game(random_predict) -> int:
@@ -31,8 +41,8 @@ def score_game(random_predict) -> int:
     """
 
     count_ls = [] # список для сохранения количества попыток
-    np.random.seed(1) # фиксируем сид для вопроизводимости
-    random_array = np.random.randint(1, 101, size=(1000)) # загадали список чисел
+    random.seed(1) # фиксируем сид для вопроизводимости
+    random_array = random.randint(1, 101, size=(1000)) # загадали список чисел
 
     for number in random_array:
         count_ls.append(random_predict(number))
